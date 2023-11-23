31679e1ae8275fc439c789708fe4c65cdfc730d6
3boodtala7m <3bood.tala7m@gmail.com>
2023-11-23 23:48:40 +0200

    last update

--- src/Decorator/BasicWeather.java
+++ src/Decorator/BasicWeather.java
@@ -2,16 +2,14 @@
 package Decorator;
 
 public class BasicWeather implements Weather{
+    private double temperature ;
+    private String description;
     public void setTemperature(double temperature) {
         this.temperature = temperature;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
-    private double temperature ;
-    private String description;
     public BasicWeather(double temperature, String description) {
         this.temperature = temperature;
         this.description = description;
